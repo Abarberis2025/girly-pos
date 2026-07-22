e59fb754f3ed8aed1b1c93ae8f2a0cdcf8bb5117
Abarberis2025 <ajbarberis1979@gmail.com>
2026-07-21 22:55:46 -0300

    Add v8 compatibility fields before payment patch

---
+++ v8-prepatch.js
@@ -0,0 +1,6 @@
+(()=>{
+ const box=document.createElement('div');
+ box.hidden=true;
+ box.innerHTML='<input id="mpFeePct"><input id="mpFixedFee"><input id="mpTaxPct"><input id="mpDelayDays"><div id="mpExample"></div>';
+ document.body.appendChild(box);
+})();
